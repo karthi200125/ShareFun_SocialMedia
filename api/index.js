762f3d8fd09b38683ba9e0e91ddfb2f890761437
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-10-18 07:25:51 -0700

    first commit of social media full stack

---
+++ api/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import path from "path";
+//securty packges
+import helmet from "helmet";
+import mongoose from "mongoose";
+import errorMiddleware from "./middleware/errorMiddleware.js";
+import router from "./routes/index.js";
+
+const __dirname = path.resolve(path.dirname(""));
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "views/build")));
+
+const PORT = process.env.PORT || 8800;
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>console.log("Mongo Db Connected"))
+.catch((err)=>console.log("Mongo db connection failed"))
+
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+app.use(router);
+
+// error middleware
+app.use(errorMiddleware);
+
+app.listen(8800, () => {
+  console.log("Server running on port: 8800");
+});
+
